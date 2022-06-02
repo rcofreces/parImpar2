56e1d01607defcca616c748fcbc7ac76cb7a816a
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-06-02 15:26:03 -0300

    Commit02/06 Corrección en console.log según observación del tutor

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 let number: number = Number(
-  prompt("Ingrese un número para saber si es par o impar")
+  prompt("Ingrese un número positivo para saber si es par o impar")
 );
 
 while (number === 0) {
-  number = Number(prompt("Ingres un número mayor a 0"));
+  number = Number(
+    prompt("Ingrese un número mayor a 0. No se permiten números negativos")
+  );
 }
 if (number % 2 === 0) {
   console.log("El número ingresado es par");
